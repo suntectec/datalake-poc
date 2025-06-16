beb2df9be2ab38c9cb42d01b50bd8de565b31c33
jagger <suntectec@163.com>
2025-06-16 10:38:42 +0800

    update seatunnel docker cluster

--- docker/paimon-poc/seatunnel-cluster/.bash
+++ docker/paimon-poc/seatunnel-cluster/.bash
@@ -1,7 +1,19 @@
+# 提交作业到集群
+# 使用docker container作为客户端
+
+# 提交任务
 docker run --name seatunnel_client \
     --network pub-network \
     -e ST_DOCKER_MEMBER_LIST=seatunnel_master:5801 \
     --rm \
     apache/seatunnel \
     ./bin/seatunnel.sh  -c config/v2.batch.config.template
+
+# 查看作业列表
+docker run --name seatunnel_client \
+    --network pub-network \
+    -e ST_DOCKER_MEMBER_LIST=seatunnel_master:5801 \
+    --rm \
+    apache/seatunnel \
+    ./bin/seatunnel.sh  -l
 
