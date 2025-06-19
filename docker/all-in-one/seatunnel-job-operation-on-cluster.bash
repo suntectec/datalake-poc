f70a3fe03e869e0429171473237a16e0279aa9e8
jagger <suntectec@163.com>
2025-06-19 16:44:55 +0800

    update docker directory path

---
+++ docker/all-in-one/seatunnel-job-operation-on-cluster.bash
@@ -0,0 +1,21 @@
+Job Operation on cluster
+use docker as a client
+submit job :
+# you need update yourself master container ip to `ST_DOCKER_MEMBER_LIST`
+docker run --name seatunnel_client \
+    --network paimon-poc_seatunnel_network \
+    -e ST_DOCKER_MEMBER_LIST=192.168.138.15:15801 \
+    --rm \
+    apache/seatunnel \
+    ./bin/seatunnel.sh  -c config/v2.batch.config.template
+
+list job
+# you need update yourself master container ip to `ST_DOCKER_MEMBER_LIST`
+docker run --name seatunnel_client \
+    --network paimon-poc_seatunnel_network \
+    -e ST_DOCKER_MEMBER_LIST=192.168.138.15:15801 \
+    --rm \
+    apache/seatunnel \
+    ./bin/seatunnel.sh  -l
+
+more command please refer user-command
